07a52d40d62eaaf8d2b6b1580339b96bde3c9e18
Nora <anes344@naver.com>
2017-05-12 18:03:54 +0900

    17.05.12_2
    설정 반은 끝난듯

---
+++ src/main/java/com/tobo/dumo/service/DumoService.java
@@ -0,0 +1,6 @@
+package com.tobo.dumo.service;
+
+public class DumoService implements DumoServiceImpl
+{
+// 실제 구현
+}
---
+++ src/main/java/com/tobo/dumo/service/DumoServiceImpl.java
@@ -0,0 +1,6 @@
+package com.tobo.dumo.service;
+
+public interface DumoServiceImpl
+{
+// 사용할 클래스 들
+}
